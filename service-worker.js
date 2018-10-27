973c452bd8265be84aca0128acaafaa7cbffe828
Viktor <vic99999@yandex.ru>
2018-10-27 16:35:23 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// https://serviceworke.rs/strategy-network-or-cache_service-worker_doc.html
+'use strict';
+
+const CACHE_NAME = 'cache-2018-10-27T11:32:46.713Z';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fromCache(event.request));
+});
+
+function precache() {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return cache.addAll([
+      './',
+    ]);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return cache.match(request);
+  });
+}
